b394ce05543e07b377c18f2f90e92c7e6a0389ce
bwsltalqma <bwsltalqma@gmail.com>
2026-06-30 16:44:39 +0300

    Delete tests directory

--- tests/example.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Verify Google Title', async ({ page }) => {
-  await page.goto('https://www.google.com');
-  await expect(page).toHaveTitle(/Google/);
-});
