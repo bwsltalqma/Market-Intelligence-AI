0b633ed154b82a4721a8ffe690f0ae734ffc1344
bwsltalqma <bwsltalqma@gmail.com>
2026-07-01 10:48:53 +0300

    Update browser.js

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,14 +1,23 @@
-import { chromium } from '@playwright/test';
+import { chromium } from "@playwright/test";
+import { config } from "../config/config.js";
 
 export async function launchBrowser() {
   const browser = await chromium.launch({
-    headless: true
+    headless: config.browser.headless
   });
 
   const page = await browser.newPage();
+
+  page.setDefaultTimeout(config.browser.timeout);
 
   return {
     browser,
     page
   };
+}
+
+export async function closeBrowser(browser) {
+  if (browser) {
+    await browser.close();
+  }
 }
