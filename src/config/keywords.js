c45d6e2627dbb68ffd52b4e1b761c5db8bdcd2a6
bwsltalqma <bwsltalqma@gmail.com>
2026-07-01 13:22:00 +0300

    Create keywords.js

---
+++ src/config/keywords.js
@@ -0,0 +1,19 @@
+export const keywords = {
+  countries: [
+    "EG"
+  ],
+
+  categories: [
+    "Home Services",
+    "Cleaning",
+    "Water Filters",
+    "Maintenance"
+  ],
+
+  searchTerms: [
+    "تنظيف خزانات",
+    "تركيب فلاتر مياه",
+    "عزل أسطح",
+    "مكافحة حشرات"
+  ]
+};
